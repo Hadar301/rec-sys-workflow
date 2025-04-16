1ff94b864af6bfd49ced5dfcde22b96a0c547c0a
Itay Katav <ikatav@redhat.com>
2025-04-16 16:34:10 +0300

    add feature_service

--- train-workflow.py
+++ train-workflow.py
@@ -4,7 +4,10 @@ import os
 from kfp import kubernetes
 from kfp.dsl import Input, Output, Dataset, Model
 
-@dsl.component(base_image="quay.io/ecosystem-appeng/rec-sys-app",)
+# IMAGE_TAG = '0.0.11'
+IMAGE_TAG = 'latest'
+
+@dsl.component(base_image=f"quay.io/ecosystem-appeng/rec-sys-app:{IMAGE_TAG}",)
 def generate_candidates(item_input_model: Input[Model], user_input_model: Input[Model], item_df_input: Input[Dataset], user_df_input: Input[Dataset]):
     from feast import FeatureStore
     from feast.data_source import PushMode
@@ -15,8 +18,11 @@ def generate_candidates(item_input_model: Input[Model], user_input_model: Input[
     import numpy as np
     from datetime import datetime
     import torch
-    
-    store = FeatureStore()
+    import os
+    print('WOWOWWO')
+    print(os.listdir('.'))
+    print(os.listdir('feature_repo/'))
+    store = FeatureStore(repo_path="feature_repo/")
     
     item_encoder = ItemTower()
     user_encoder = UserTower()
@@ -67,9 +73,8 @@ def generate_candidates(item_input_model: Input[Model], user_input_model: Input[
     store.push('user_items_push_source', user_items_df, to=PushMode.ONLINE)
 
 
-@dsl.component(base_image="quay.io/ecosystem-appeng/rec-sys-app")
-def train_model(item_df_input: Input[Dataset], user_df_input: Input[Dataset], interaction_df_input: Input[Dataset], item_output_model: Output[Model], user_output_model: Output[Model]):
-    from models.two_tower import TwoTowerModel
+@dsl.component(base_image=f"quay.io/ecosystem-appeng/rec-sys-app:{IMAGE_TAG}",)
+def train_model(item_df_input: Input[Dataset], user_df_input: Input[Dataset], interaction_df_input: Input[Dataset], neg_interaction_df_input:Input[Dataset], item_output_model: Output[Model], user_output_model: Output[Model]):
     from models.user_tower import UserTower
     from models.item_tower import ItemTower
     from models.train_two_tower import train_two_tower
@@ -80,31 +85,33 @@ def train_model(item_df_input: Input[Dataset], user_df_input: Input[Dataset], in
     item_df = pd.read_parquet(item_df_input.path)
     user_df = pd.read_parquet(user_df_input.path)
     interaction_df = pd.read_parquet(interaction_df_input.path)
+    neg_interaction_df = pd.read_parquet(neg_interaction_df_input.path)
 
     item_encoder = ItemTower(dim)
     user_encoder = UserTower(dim)
-    two_tower_model = TwoTowerModel(item_tower=item_encoder, user_tower=user_encoder)
-    train_two_tower(two_tower_model, item_df, user_df, interaction_df)
+    train_two_tower(item_encoder, user_encoder, item_df, user_df, interaction_df, neg_interaction_df)
     
     torch.save(item_encoder.state_dict(), item_output_model.path)
     torch.save(user_encoder.state_dict(), user_output_model.path)
     item_output_model.metadata['framework'] = 'pytorch'
     user_output_model.metadata['framework'] = 'pytorch'
     
-
 @dsl.component(
-    base_image="quay.io/ecosystem-appeng/rec-sys-app") #TODO change image to rec-sys-feast-repository
-    # packages_to_install=["feast[postgres]>=0.46.0","pandas"],)
-def load_data_from_feast(item_df_output: Output[Dataset], user_df_output: Output[Dataset], interaction_df_output: Output[Dataset]):
+    base_image=f"quay.io/ecosystem-appeng/rec-sys-app:{IMAGE_TAG}", packages_to_install=['grpcio'])
+def load_data_from_feast(item_df_output: Output[Dataset], user_df_output: Output[Dataset], interaction_df_output: Output[Dataset], neg_interaction_df_output: Output[Dataset]):
     from feast import FeatureStore
-    from datetime import datetime, timedelta
+    from datetime import datetime
     import pandas as pd
-    # TODO make sure the the image have updated feature_store.yaml
-    store = FeatureStore()
+    import os
+    print('WOWOWWO')
+    print(os.listdir('.'))
+    print(os.listdir('feature_repo/'))
+    store = FeatureStore(repo_path="feature_repo/")
     # load feature services
     item_service = store.get_feature_service("item_service")
     user_service = store.get_feature_service("user_service")
     interaction_service = store.get_feature_service("interaction_service")
+    neg_interactions_service = store.get_feature_service('neg_interaction_service')
 
     num_users = 1_000
     n_items = 5_000
@@ -127,22 +134,27 @@ def load_data_from_feast(item_df_output: Output[Dataset], user_df_output: Output
         }
     )
     # Select which item-user interactions to use for the training
-    item_user_interactions_df = pd.read_parquet('./data/interactions_item_user_ids.parquet')
+    item_user_interactions_df = pd.read_parquet('./feature_repo/data/interactions_item_user_ids.parquet')
+    item_user_neg_interactions_df = pd.read_parquet('./feature_repo/data/neg_interactions_item_user_ids.parquet')
     item_user_interactions_df['event_timestamp'] = datetime(2025, 1, 1)
+    item_user_neg_interactions_df['event_timestamp'] = datetime(2025, 1, 1)
 
     # retrive datasets for training
     item_df = store.get_historical_features(entity_df=item_entity_df, features=item_service).to_df()
     user_df = store.get_historical_features(entity_df=user_entity_df, features=user_service).to_df()
     interaction_df = store.get_historical_features(entity_df=item_user_interactions_df, features=interaction_service).to_df()
+    neg_interaction_df = store.get_historical_features(entity_df=item_user_neg_interactions_df, features=neg_interactions_service).to_df()
     
     # Pass artifacts
     item_df.to_parquet(item_df_output.path)
     user_df.to_parquet(user_df_output.path)
     interaction_df.to_parquet(interaction_df_output.path)
+    neg_interaction_df.to_parquet(neg_interaction_df_output.path)
     
     item_df_output.metadata['format'] = 'parquet'
     user_df_output.metadata['format'] = 'parquet'
     interaction_df_output.metadata['format'] = 'parquet'
+    neg_interaction_df_output.metadata['format'] = 'parquet'
 
     
 @dsl.pipeline(name=os.path.basename(__file__).replace(".py", ""))
@@ -155,7 +167,8 @@ def batch_recommendation():
     train_model_task = train_model(
         item_df_input=load_data_task.outputs['item_df_output'],
         user_df_input=load_data_task.outputs['user_df_output'],
-        interaction_df_input=load_data_task.outputs['interaction_df_output']
+        interaction_df_input=load_data_task.outputs['interaction_df_output'],
+        neg_interaction_df_input=load_data_task.outputs['neg_interaction_df_output'],
     ).after(load_data_task)
     train_model_task.set_caching_options(False)
     
